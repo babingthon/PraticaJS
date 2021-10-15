fc300a4042640742249ff2a7d055ccd74bf8f9a7
babingthon <61935008+babingthon@users.noreply.github.com>
2021-10-14 21:19:15 -0300

    lyricsSearch

---
+++ lyricsSearchJS/app.js
@@ -0,0 +1,90 @@
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#search');
+const songsContainer = document.querySelector('#songs-container');
+const prevAndMNextContainer = document.querySelector('#prev-and-next-container');
+
+const apiUrl = `https://api.lyrics.ovh`;
+
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await response.json();
+
+    insertSongsIntoPage(data);
+
+};
+
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(song => `
+    <li class="song">
+        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver Letra</button>
+        </li>
+    `).join('');
+
+    if(songsInfo.prev || songsInfo.next) {
+        prevAndMNextContainer.innerHTML = `
+        ${songsInfo.prev ? `<button class="btn" onclick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+        ${songsInfo.next ? `<button class="btn" onclick="getMoreSongs('${songsInfo.next}')">Proximas</button>` : ''}
+        `
+        return
+    }
+    prevAndMNextContainer.innerHTML = '';
+}
+
+const fetchSongs = async term => {
+    const response = await fetch(`${apiUrl}/suggest/${term}`);
+    const data = await response.json();
+    console.log(data);
+
+    if(Object.values(data).length > 0) {
+        
+        insertSongsIntoPage(data)
+    } else {
+        songsContainer.innerHTML = '';
+        songsContainer.innerHTML = `<li class="warning-message">Artista não Encontrado.</li>`;
+    }
+    
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const searchTerm = searchInput.value.trim();
+
+    if(!searchTerm) {
+        songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo válido.</li>`
+        return;
+    }
+
+    fetchSongs(searchTerm);
+});
+
+const fetchLyrics = async (artist, songTitle) => {
+    const response = await fetch(`${apiUrl}/v1/${artist}/${songTitle}`);
+    const data = await response.json();
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+    
+    songsContainer.innerHTML = `
+    <li class="lyrics-container">
+    <h2><strong>${songTitle}</strong> - ${artist}</h2>
+    <p class="lyrics">${lyrics}</p>
+    </li>
+    `
+
+};  
+
+songsContainer.addEventListener('click', event => {
+    const clickedElement = event.target;
+
+    if(clickedElement.tagName === 'BUTTON') {
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-song-title');
+        
+        prevAndMNextContainer.innerHTML = '';
+        fetchLyrics(artist, songTitle);
+    }
+
+
+
+
+});
